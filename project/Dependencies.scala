4e5b29c8182e50fe9e93ebbc2ae1d23c19ab5faf
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-07-30 19:34:04 +0300

    Add Scala 3 build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,9 @@ object Dependencies {
   object Versions {
     val scala212 = "2.12.14"
     val scala213 = "2.13.6"
+    val scala3 = "3.0.1"
 
-    val trace4cats = "0.12.0-RC2"
+    val trace4cats = "0.12.0-RC2+18-2de6b68e"
 
     val embeddedRedis = "0.7.3"
     val redis4cats = "1.0.0"
@@ -21,7 +22,7 @@ object Dependencies {
   lazy val embeddedRedis = "it.ozimov"            % "embedded-redis"      % Versions.embeddedRedis
   lazy val redis4cats = "dev.profunktor"         %% "redis4cats-effects"  % Versions.redis4cats
   lazy val redis4catsLog4cats = "dev.profunktor" %% "redis4cats-log4cats" % Versions.redis4cats
-  lazy val scaffeine = "com.github.blemale"      %% "scaffeine"           % Versions.scaffeine
+  lazy val scaffeine = ("com.github.blemale"     %% "scaffeine"           % Versions.scaffeine).cross(CrossVersion.for3Use2_13)
 
   lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor
