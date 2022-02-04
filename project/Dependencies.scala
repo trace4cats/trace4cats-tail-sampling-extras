89628bade201994ec9254194b47d8c2ee5d0094f
Scala Steward <me@scala-steward.org>
2022-02-04 14:53:56 +0100

    Update redis4cats-effects, ... to 1.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     val catsEffect = "3.3.5"
     val embeddedRedis = "0.7.3"
-    val redis4cats = "1.0.0"
+    val redis4cats = "1.1.1"
     val caffeine = "2.9.3"
     val collectionCompat = "2.6.0"
 
